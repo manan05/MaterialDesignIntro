44a966e6eb233516a8a41838c99417e2207ea0b4
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-10-05 17:24:16 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/materialdesignintro/MainActivity.java
+++ app/src/main/java/com/example/materialdesignintro/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.materialdesignintro;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -21,19 +23,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Alert Dialog is used to give alerts to the user
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("Hello From AlertDialog") //The title of the alert dialog
+                .setMessage("I am a generic alert Dialog!") // The message beneath the Title
+                .setCancelable(false) //If we want to allow it to be cancellable when pressed elsewhere
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    // can set +ve, -ve, neutral buttons for the response
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Hello, I am a Toast",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }).create();
 
         final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Hello From Basic Activity", Snackbar.LENGTH_INDEFINITE)
-                        .setAction("Undo", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Toast.makeText(MainActivity.this, "Hello, I am a Toast", Toast.LENGTH_SHORT).show();
-                            }
-                        }).show();
+                alertDialog.show();
+//                Snackbar.make(view, "Hello From Basic Activity", Snackbar.LENGTH_INDEFINITE) // used to give messages
+//                        .setAction("Undo", new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//
+//                            }
+//                        }).show();
             }
+            //Advantage of snackbar over toast
+            // = That we can assign actions to the snackbars but not to the toasts
         });
     }
 
